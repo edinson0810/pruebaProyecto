90ed6c3228ad0a634b73e8b2a914f43727d9e641
edinson0810 <lache081015@gmail.com>
2025-06-07 14:16:51 -0500

    clave

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2';
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Yamir081015',
+  password: '#Aprendiz2024',
   database: 'restaurant_system'
 });
 
