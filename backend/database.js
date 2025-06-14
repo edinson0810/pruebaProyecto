37a4f8e26a42f277ea931db7f9bf98bceee58789
edinson0810 <lache081015@gmail.com>
2025-06-13 19:51:54 -0500

    modificadicon contrasena sql

--- backend/database.js
+++ backend/database.js
@@ -4,7 +4,7 @@ import mysql from 'mysql2';
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Yamir081015',
+  password: '#Aprendiz2024',
   database: 'restaurant_system'
 });
 
