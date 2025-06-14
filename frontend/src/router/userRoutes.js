660ae4ef5a466f33555b5be79ed9e48dceece406
edinson0810 <lache081015@gmail.com>
2025-06-13 20:22:52 -0500

    imagenes

--- frontend/src/router/userRoutes.js
+++ frontend/src/router/userRoutes.js
@@ -1,10 +1,10 @@
-// BACKEND/src/routes/userRoutes.js
+
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const authMiddleware = require('../middleware/authMiddleware'); // <--- Asegúrate de que esta ruta sea correcta y que tu middleware exista
+const authMiddleware = require('../middleware/authMiddleware');
 
-// Proteger estas rutas para que solo usuarios autenticados (y con el rol adecuado) puedan acceder
+
 router.get('/', authMiddleware, userController.getUsers);
 router.post('/', authMiddleware, userController.createUser);
 router.put('/:id', authMiddleware, userController.updateUser);
